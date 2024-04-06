ce46ccf62018e5f788de8295c6f4a321f87da9f8
SebastianUVG <gar22291@uvg.edu.gt>
2024-04-06 17:03:12 -0600

    Inicio de hoja
    Creacion de menu para ingresar las rutas de los archivos para leer lo que corresponde

--- hoja7/src/main/java/uvg/edu/gt/App.java
+++ hoja7/src/main/java/uvg/edu/gt/App.java
@@ -1,5 +1,7 @@
 package uvg.edu.gt;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner sc = new Scanner(System.in);
+        //boolean badera = true; 
+
+        
+            System.out.println("Bienvenido al diccionario");
+            System.out.println("Por favor ingrese el nombre del archivo que quiere traducir (nombreDelArchivo.txt): ");
+            String nombre = sc.nextLine().trim();
+            System.out.println("Por favor ingrese el nombre del diccionario que quiere utilizar(nombreDelArchivo.txt): ");
+            String diccionario = sc.nextLine().trim();
+
+        
     }
 }
